0958bb47f804751243f59485b9f983f2591ab51f
Gabriel Piske <gpiske2016@gmail.com>
2025-04-08 01:51:05 -0300

    Ajuste no index.ts

---
+++ Veiculo.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Veiculo = void 0;
+class Veiculo {
+    constructor() {
+        this.marca = 'Padrão';
+        this.modelo = 'Padrão';
+        this.potencia = 0;
+        this.numeroMarchas = 5;
+        this.marchaAtual = 0;
+        this.velocidade = 0;
+    }
+}
+exports.Veiculo = Veiculo;
