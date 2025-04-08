ea7a18e44ea42c4c63b73900b62e4a9759db4f2e
Juan Quinzen <juanquinzen05@gmail.com>
2025-04-08 04:23:30 -0300

    Alterações no código

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const gerarSenha = require('./gerarSenha');
 const validarSenha = require('./validarSenha');
 
-// Função adicionada: soma dois números
+
 function soma(a, b) {
     return a + b;
 }
-// Função adicionada: valida se a entrada é válida
+
 function validarEntrada(valor) {
     return valor !== null && valor !== undefined && valor !== '';
 }
-// Testes rápidos
+
 console.log("Resultado da soma(3, 5):", soma(3, 5));
 console.log("Entrada 'teste' válida?:", validarEntrada("teste"));
 
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 
-// Função adicionada: soma dois números
+
 function soma(a: number, b: number): number {
     return a + b;
 }
 
-// Função adicionada: valida se a entrada é válida
+
 function validarEntrada(valor: any): boolean {
     return valor !== null && valor !== undefined && valor !== '';
 }
 
-// Testes rápidos
+
 console.log("Resultado da soma(3, 5):", soma(3, 5));
 console.log("Entrada 'teste' válida?:", validarEntrada("teste"));
